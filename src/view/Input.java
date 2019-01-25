a96b0e08b8622fc7699da0db27200bf3607c9a47
Niklas <niklas.grethler@web.de>
2019-01-25 11:17:56 +0100

    Update Input.java

--- src/view/Input.java
+++ src/view/Input.java
@@ -4,5 +4,16 @@ import java.util.Scanner;
 public class Input {
     Scanner scan = new Scanner(System.in);
 
+    public String inputReader(String input){
 
+        String patternTakeKey = ".*take.*[kK]ey.*";
+
+        String userInput = scan.nextLine();
+
+        if(userInput.matches(patternTakeKey)){
+            return "takeKey";
+        }
+
+        return "";
+    }
 }
