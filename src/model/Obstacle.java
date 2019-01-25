27e618bfc304bce83a3e65e3ac47cff9730e3371
simone273 <maag.s@web.de>
2019-01-25 12:17:30 +0100

    initiate obstacle
    initiate getter and setter, check if one is using the correct item, resolved true or false, implement attributes

--- src/model/Obstacle.java
+++ src/model/Obstacle.java
@@ -1,21 +1,59 @@
 package model;
 
 public class Obstacle {
-	
-	public String description;
-	public String resolution;
-	public boolean resolved = false;
-	
+
+
+	private String description;
+
+
+	private String resolution;
+
+
+	private boolean resolved = false;
+
+
 	private Item requiredItem;
-	
-	public Obstacle(String description, Item requiredItem) {
+
+	// constructor
+	public Obstacle(String description, Item requiredItem, String resolution) {
 		this.description = description;
 		this.requiredItem = requiredItem;
+		this.resolution = resolution;
 	}
 	
 	public boolean tryToUseItem(Item itemToTry) {
 		// TODO check for correct Item, return correct value
-		return false;
+		// return true if correct item
+		if (requiredItem.equals(itemToTry)){
+			return true;
+		}
+		else{
+			return false;
+		}
+
 	}
 
+   // getter description
+	public String getDescription() {
+		return description;
+	}
+
+	//getter resolution
+	public String getResolution() {
+		return resolution;
+	}
+
+	// getter resolved
+	public boolean isResolved() {
+		return resolved;
+	}
+
+   // getter required
+	public Item getRequiredItem() {
+		return requiredItem;
+	}
+
+	public void resolved (){
+		this.resolved= true;
+	}
 }
