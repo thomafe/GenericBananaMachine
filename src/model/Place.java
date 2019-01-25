3b48484fbe47077bfed7f08c94003050f89e9714
simone273 <maag.s@web.de>
2019-01-25 12:02:25 +0100

    Revert "Merge branch 'master' of https://github.com/thomafe/GenericBananaMachine"
    This reverts commit 9297a30aaf0beab5a689c680a534bc12a1621579, reversing
    changes made to 83b7db763c8d8958512b39b80c1ac28134d18be5.

--- src/model/Place.java
+++ src/model/Place.java
@@ -4,50 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Place {
-
-
-
-	// Attribute
+	
 	public String description;
-
-
-
 	public List<Passage> passages;
-
-
 	public List<Item> itemsOnTheFloor;
-
-	// constructor
+	
 	public Place() {
 		passages = new ArrayList<>();
 		itemsOnTheFloor = new ArrayList<>();
 	}
-
-	// noch ein constructor für place mit inhalt diesmal
+	
 	public Place(String description) {
 		this();
-		this.description= description;
-
-		// TODO set passages
-	}
-
-	// getter description
-	public String getDescription() {
-		return description;
-	}
-
-	// getter für passages
-	public List<Passage> getPassages() {
-		return passages;
-	}
-
-	// getter and setter für items on the floor
-	public List<Item> getItemsOnTheFloor() {
-		return itemsOnTheFloor;
-	}
-
-	public void setItemsOnTheFloor(List<Item> itemsOnTheFloor) {
-		this.itemsOnTheFloor = itemsOnTheFloor;
+		// TODO set description and passages
 	}
 
 }
