bed3cac5bc847b1b5012e6a9e78031be1c344648
simone273 <maag.s@web.de>
2019-01-25 11:18:55 +0100

    implement item
    impelment methods and attributes

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,12 +1,36 @@
 package model;
 
 public class Item {
-	
-	public String description;
-	public boolean consumed = false;
-	
+
+
+
+	// Attribute
+	private String description;
+
+
+
+
+	private boolean consumed = false;
+
+	// constructor (zum objekte erstellen)
 	public Item(String description) {
-		
+
+		this.description = description;
+
+
+	}
+	// getter, public methode gibt attribut zurück
+	public String getDescription() {
+		return description;
+	}
+
+	public boolean isConsumed() {
+		return consumed;
+	}
+
+	// methode, wenn item consumed dann true
+	public void consume() {
+		this.consumed = true;
 	}
 
 }
