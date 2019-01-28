3cd3eb498856665a119e8131ada39d31c11be6e5
thomafe <46815897+thomafe@users.noreply.github.com>
2019-01-28 12:33:44 +0100

    debug GameLoop

--- src/control/GameController.java
+++ src/control/GameController.java
@@ -3,82 +3,73 @@ package control;
 import model.*;
 import view.*;
 
-
 import model.Character;
 import java.util.Scanner;
 
 public class GameController {
 
-    public static void main (String[] args) {
+	public static void main(String[] args) {
 
-        // initialize basic game settings
-        initGame();
+		// initialize basic game settings
+		initGame();
 
+	}
 
+	private static void initGame() {
 
-    }
+		Scanner scanner = new Scanner(System.in);
 
-    private static void initGame () {
+		Place entrance = new Place("Entrance", "This is your starting area.");
+		Place secondRoom = new Place("Hall of Doom", "This is the final Boss Room...not. It just sounds cool.");
+		Place thirdRoom = new Place("Lighthouse", "You can't see anything in here because the light is blinding.");
 
-        Scanner scanner = new Scanner(System.in);
+		Character character = new Character(entrance);
 
+		Passage pas1 = new Passage("Door of Doom", "This Door seems to be very heavy and doomed", entrance, secondRoom);
+		Passage pas2 = new Passage("snake pit", "You are greeted by the lovely sound of zzzzzzzzz", secondRoom,
+				thirdRoom);
+		Item item1 = new Item("Lightsaber", "This is a powerful jedi melee weapon.");
+		Item item2 = new Item("Banana", "This is a powerful fruit which makes you feel like a monkey.");
+		entrance.addItemOnTheFloor(item1);
+		secondRoom.addItemOnTheFloor(item2);
 
-        Place entrance = new Place("Entrance", "This is your starting area.");
-        Place secondRoom = new Place("Hall of Doom", "This is the final Boss Room...not. It just sounds cool.");
-        Place thirdRoom = new Place("Lighthouse", "You can't see anything in here because the light is blinding.");
+		entrance.addPassage(pas1);
+		secondRoom.addPassage(pas1);
+		secondRoom.addPassage(pas2);
 
-        Character character = new Character(entrance);
+		Control ctrl = new Control(character);
 
-        Passage pas1 = new Passage("Door of Doom", "This Door seems to be very heavy and doomed", entrance, secondRoom);
-        Passage pas2 = new Passage("snake pit", "You are greeted by the lovely sound of zzzzzzzzz", secondRoom, thirdRoom);
-        Item item1 = new Item("Lightsaber", "This is a powerful jedi melee weapon.");
-        Item item2 = new Item("Banana", "This is a powerful fruit which makes you feel like a monkey.");
-        entrance.addItemOnTheFloor(item1);
-        secondRoom.addItemOnTheFloor(item2);
+		// TODO output describe place
+		Output output = new Output(ctrl);
+		Input input = new Input(output, ctrl);
 
-        entrance.addPassage(pas1);
-        secondRoom.addPassage(pas2);
+		output.greeting();
+		do {
+			output.lookAtCurrentPlace();
 
-        Control ctrl = new Control(character);
+			// TODO output list options
+			output.listOptions();
 
-        // TODO output describe place
-        Output output= new Output(ctrl);
+			// TODO input take
 
-        output.greeting();
-        output.lookAtCurrentPlace();
+			input.readInput();
 
-        // TODO output list options
-         output.listOptions();
+		} while (true);
 
-         // TODO input take
+		/*
+		 * 
+		 * // character tries to take item from floor character.takeItem(item1);
+		 * 
+		 * // character calls List of Inventory for(Item item:
+		 * character.getItemsInInventory()) {
+		 * 
+		 * System.out.println(item.getName()); }
+		 */
 
-        Input input = new Input(output);
+		// character tries to move
+		// TODO: character tries to move and if success he moves to the next place.
 
-        while (true){
-            input.readInput();
-        }
-
-
-
-        /*
-
-        // character tries to take item from floor
-        character.takeItem(item1);
-
-         // character calls List of Inventory
-        for(Item item: character.getItemsInInventory()) {
-
-            System.out.println(item.getName());
-        }
-        */
-
-        // character tries to move
-        // TODO: character tries to move and if success he moves to the next place.
-
-
-
-
-        // TODO: create further objects and connections between Places and Obstacles
-    }
+		// TODO: create further objects and connections between Places and Obstacles
+	}
 
 }
