d76c4059c771acc3ca06f9d9267b42aca3a4f293
Lehmeti <lehmeier.tim@t-online.de>
2019-01-28 14:41:02 +0100

    Revert "Merge branch 'master' of https://github.com/thomafe/GenericBananaMachine"
    This reverts commit 66184c551ea71e40038ce2e754e7d99de296abe5, reversing
    changes made to 0a5099e8908de7f818ee77dd9cdae410d92ed7fd.

--- src/model/Passage.java
+++ src/model/Passage.java
@@ -1,91 +1,96 @@
 package model;
 
 // class mit name passage
-public class Passage extends GameObject {
+public class Passage extends GameObject{
 
 
-  // attribute
-  private Place[] connectingRooms = new Place[2];
 
-  private Obstacle obstacle = null;
+	// attribute
+	private Place[] connectingRooms = new Place[2];
+
+	private Obstacle obstacle = null;
 
 
-  /**
-   * Constructor.
-   *
-   * @param name String
-   * @param description String
-   * @param place1 Place
-   * @param place2 Place
-   */
-  // constructor, kein rückgabewert, name so wie klasse, initialisiert classenattribute
-  public Passage(String name, String description, Place place1, Place place2) {
-    super(name, description);
-    this.connectingRooms[0] = place1;
-    this.connectingRooms[1] = place2;
-    place1.addPassage(this);
-    place2.addPassage(this);
-  }
+	/**
+	 * Constructor.
+	 *
+	 * @param name String
+	 * @param description String
+	 * @param place1 Place
+	 * @param place2 Place
+	 */
+	// constructor, kein rückgabewert, name so wie klasse, initialisiert classenattribute
+	public Passage(String name, String description, Place place1, Place place2) {
+		super(name, description);
+		this.connectingRooms[0] = place1;
+		this.connectingRooms[1]= place2;
+		place1.addPassage(this);
+		place2.addPassage(this);
+	}
 
-  /**
-   * Returns next Place after accessing the passage.
-   *
-   * @param comingFromPlace Place
-   * @return Object
-   */
-  public Place usePassage(Place comingFromPlace) {
-    // zurückgeben in welchem raum man ist
-    if (comingFromPlace.equals(connectingRooms[0])) {
-      return connectingRooms[1];
-    } else if (comingFromPlace.equals(connectingRooms[1])) {
-      return connectingRooms[0];
-    } else {
-      return null;
-    }
-  }
+	/**
+	 * Returns next Place after accessing the passage.
+	 *
+	 *
+	 * @param comingFromPlace Place
+	 * @return Object
+	 */
+	public Place usePassage(Place comingFromPlace) {
+		// zurückgeben in welchem raum man ist
+		if (comingFromPlace.equals(connectingRooms[0])) {
+			return connectingRooms[1];
+		}
+		else if (comingFromPlace.equals(connectingRooms[1])) {
+			return connectingRooms[0];
+		}
+		else {
+			return null;
+		}
+	}
 
-  // getter and setter for obstacle
+	// getter and setter for obstacle
 
-  /**
-   * Getter for obstacle.
-   *
-   * @return Object
-   */
-  public Obstacle getObstacle() {
-    return obstacle;
-  }
+	/**
+	 * Getter for obstacle.
+	 *
+	 * @return Object
+	 */
+	public Obstacle getObstacle() {
+		return obstacle;
+	}
 
-  /**
-   * Setter for obstacle. Sets committed obstacle to
-   *
-   * @param obstacle Obstacle
-   */
-  public void setObstacle(Obstacle obstacle) {
-    this.obstacle = obstacle;
-  }
+	/**
+	 * Setter for obstacle.
+	 * Sets committed obstacle to
+	 *
+	 * @param obstacle Obstacle
+	 */
+	public void setObstacle(Obstacle obstacle) {
+		this.obstacle = obstacle;
+	}
 
-  /**
-   * Getter for connectingRooms.
-   *
-   * @return Object Array
-   */
-  // getter for connecting rooms
-  public Place[] getConnectingRooms() {
-    return connectingRooms;
-  }
+	/**
+	 * Getter for connectingRooms.
+	 *
+	 * @return Object Array
+	 */
+	// getter for connecting rooms
+	public Place[] getConnectingRooms() {
+		return connectingRooms;
+	}
 
-  /**
-   * if this has an Obstacle, return true, else return false.
-   *
-   * @return boolean
-   */
-  public boolean hasObstacle() {
+	/**
+	 * if this has an Obstacle, return true, else return false.
+	 *
+	 * @return boolean
+	 */
+	public boolean hasObstacle () {
 
-    if (obstacle != null) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
+		if(obstacle != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
