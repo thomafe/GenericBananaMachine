34fb29e1e5841cda783599198124d417062b32c8
Lehmeti <lehmeier.tim@t-online.de>
2019-02-01 13:10:23 +0100

    Create CreateWorld.java
    Create basic World Creator Class.

---
+++ src/control/CreateWorld.java
@@ -0,0 +1,8 @@
+package control;
+
+/**
+ * Take information from XmlParser and use it to create several objects and relations between.
+ */
+public class CreateWorld {
+  // TODO: create objects similar to current Control main method
+}
