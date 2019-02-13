edab548e5426f9051ca85aa682e0681601dd52c0
Niklas <niklas.grethler@web.de>
2019-02-13 11:43:30 +0100

    Update GameControl.java

--- src/control/GameControl.java
+++ src/control/GameControl.java
@@ -310,11 +310,7 @@ public class GameControl {
     if (character.getCurrentPlace().getName().equals("Ship of Coastguard")) {
       out.goodEnding();
 
-      if (playAgain()) {
-        //restart game
-      } else {
-        gameIsRunning = false;
-      }
+      restartGame = playAgain();
     }
   }
 
@@ -335,11 +331,7 @@ public class GameControl {
        * control.runGame(); } else { out.doOutput("Thanks for playing! See you later."); }
        */
 
-      if (playAgain()){
-        //restart game
-      } else {
-        gameIsRunning = false;
-      }
+      restartGame = playAgain();
     }
   }
 
